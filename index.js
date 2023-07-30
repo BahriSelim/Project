23929b0bc069d8099ab6ab8772589db3a5734e04
BahriSelim <140980223+BahriSelim@users.noreply.github.com>
2023-07-30 22:10:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ const eroor = document.querySelector('.eroor');
 search.addEventListener("click",()=>{
     const APIKEY="fcb29f6d08d9e7e9fbb988146ea2f732"
     const city=document.querySelector(".searchbox input").value
+    const img=document.querySelector(".weatherbox img")
+    const temp=document.querySelector(".weatherbox .temp")
+    const desc=document.querySelector(".weatherbox .desc")
+    const wind=document.querySelector(".weatherdetails .wind p")
+    const hum=document.querySelector(".weatherdetails .humidity p")
     if(city===""){
         return;
 
     }
+    else if(city=="hajji"){
+        
+        temp.innerHTML= "99999C°";
+        desc.innerHTML="VERT HOT";
+        weatherdetails.style.opacity="0";
+        img.src="img/hajji.jpg"
+        con.style.height="500px";
+
+    }
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKEY}`)
     .then(res=>res.json()).then(json=>{
-        if(json.cod==="404"){
+        if(json.cod==="404" && city!="hajji"){
             eroor.style.display="block";
             con.style.height="500px";
             eroor.classList.add(".fadein");
